59243e6e16b472a9f610e7564abc4f2f9d54e412
Thang Nguyen <thang.nguyen.dinh026317@gmail.com>
2021-12-05 15:13:02 +0700

    Clean up

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -12,12 +12,12 @@ const HeroSection = () => {
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
-          GET STARTED
+          GET STARTED!
         </Button>
 
-        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+        {/* <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
           WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+        </Button> */}
       </div>
     </div>
   )
